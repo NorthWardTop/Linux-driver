2d934b9c8d7deaf9e348bf3a851d698fd8d420a4
NorthWardTop <2394783277@qq.com>
2019-04-28 15:14:08 +0800

    printk打印优先级

--- hello_module/.tmp_versions/hello_kernel.mod
+++
@@ -1,2 +0,0 @@
-/mnt/hgfs/SI/EmbeddedStu/Linux-driver/hello_module/hello_kernel.ko
-/mnt/hgfs/SI/EmbeddedStu/Linux-driver/hello_module/hello_kernel.o
--- hello_module/hello_kernel.c
+++ hello_module/hello_kernel.c
@@ -2,31 +2,42 @@
  * @Author: northward
  * @Github: https://github.com/northwardtop
  * @LastEditors: northward
- * @Description: Description
+ * @Description: 
+ * printk文件内容:各种消息的优先级
+ * 1.控制台打印级别:当默认消息级别高于该值时,消息就能打印
+ * 2.默认消息日志级别:基准
+ * 
+ * 
  * @Date: 2019-04-27 19:59:54
- * @LastEditTime: 2019-04-27 22:16:08
+ * @LastEditTime: 2019-04-28 15:14:00
  */
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/miscdevice.h>
 
+#define DEVICE_NAME		"led"
 
 
 
 
 
-
+// static struct miscdev led_dev=
+// {
+// 	.minor=MISC_DYNAMIC_MINOR,
+// 	.name=DEVICE_NAME,
+// 	.fops=&led_dev_fops,
+// };
 
 static int __init led_init(void)
 {
-	while(1);
-	printk("GEC6818 LED installed, (c) 2019 SLsiAP\n");
+	//while(1);
+	printk("fffffffGEC6818 LED installed, (c) 2019 SLsiAP\n");
 	return 0;
 }
 
 static void __exit led_exit(void)
 {
-	printk(KERN_WARNING"GEC6818 LED removed, (c) 2019 SLsiAP\n");
+	printk("GEC6818 LED removed, (c) 2019 SLsiAP\n");
 }
 
 module_init(led_init);
